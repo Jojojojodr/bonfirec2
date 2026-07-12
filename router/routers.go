7292629d192cb8e847434a7d5719856d257699d6
Jordy de Ron <jordy@tronsoft.nl>
2026-07-12 18:18:50 +0200

    updated api router

--- router/routers.go
+++ router/routers.go
@@ -26,21 +26,31 @@ func SetupRouter(router *gin.Engine) *gin.Engine {
 func SetupApiRouter(router *gin.Engine) *gin.Engine {
 	api := router.Group("/api")
 	api.GET("/health", controller.GetHealth)
-	api.GET("/grunts", controller.GetGrunts)
 	api.GET("/notifications", controller.GetNotifications)
+	api.GET("/messages", controller.GetLatestMessages)
 
 	tasks := api.Group("/tasks")
 	tasks.GET("/", controller.GetTasks)
 	tasks.POST("/", controller.CreateTask)
 
+	grunts := api.Group("/grunts")
+	grunts.GET("/", controller.GetGrunts)
+
+	grunt := grunts.Group("/g")
+	grunt.GET("/", controller.GetGruntById)
+	
+	messages := grunt.Group("/messages")
+	messages.GET("/", controller.GetGruntMessages)
+	messages.POST("/m", controller.SendGruntMessage)
+
 	listeners := api.Group("/listeners")
 	listeners.GET("/", controller.GetListeners)
 	listeners.POST("/", controller.CreateListener)
 
-	messages := api.Group("/messages")
-	messages.GET("/", controller.GetLatestMessages)
-	messages.GET("/grunt", controller.GetGruntMessages)
-	messages.POST("/grunt", controller.SendGruntMessage)
+	listener := listeners.Group("/l")
+	listener.GET("/", controller.GetListenerById)
+	listener.POST("/start", controller.StartListener)
+	listener.POST("/stop", controller.StopListener)
 
 	return router
 }
